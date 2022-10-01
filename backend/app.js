e177311063eb2bfd0ce4b9d5048178afad88eee9
natkru <n9215841072@gmail.com>
2022-10-01 12:43:26 +0300

    fd

--- backend/app.js
+++ backend/app.js
@@ -33,8 +33,8 @@ const allowedCors = [
   'http://localhost:3001',
   'http://zvyagina.students.nomoredomains.club',
   'httS://zvyagina.students.nomoredomains.club',
-  'http://api.zvyagina.students.nomoredomains.club',
-  'https://api.zvyagina.students.nomoredomains.club',
+  // 'http://api.zvyagina.students.nomoredomains.club',
+  // 'https://api.zvyagina.students.nomoredomains.club',
 ];
 
 app.use(cors({
